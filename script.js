f531d6a2d6113e38fdd83984ae96c2f784b2ae49
Marcelo Botura <mbsfoz@gmail.com>
2024-08-20 22:35:35 -0300

    Atualização
    Atualização java

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ function searchPDF() {
     alert('Você buscou por: ' + query);
     // Aqui você pode adicionar a lógica para buscar o PDF no servidor
 }
+
+function filterByCourse() {
+    let filter = document.getElementById('courseFilter').value;
+    let items = document.querySelectorAll('.list ul li');
+
+    items.forEach(item => {
+        if (filter === 'all' || item.classList.contains(filter)) {
+            item.style.display = '';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
