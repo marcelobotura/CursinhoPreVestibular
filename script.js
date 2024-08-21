1c4dce230095c7f6a017d3e9340377cfef3c3d0c
Marcelo Botura <mbsfoz@gmail.com>
2024-08-20 22:24:08 -0300

    Add
    HTML, JavaScript, CSS

---
+++ script.js
@@ -0,0 +1,5 @@
+function searchPDF() {
+    let query = document.getElementById('search').value;
+    alert('Você buscou por: ' + query);
+    // Aqui você pode adicionar a lógica para buscar o PDF no servidor
+}
