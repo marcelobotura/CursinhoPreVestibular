62452cf2b31d0336391240e56df5a6a38024d581
Marcelo Botura <mbsfoz@gmail.com>
2024-08-20 22:39:51 -0300

    atualização css java
    arrumando o filtro

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function searchPDF() {
     let query = document.getElementById('search').value;
-    alert('Você buscou por: ' + query);
-    // Aqui você pode adicionar a lógica para buscar o PDF no servidor
+
+    if (query.trim() !== "") {
+        document.getElementById('results').classList.remove('hidden');
+        alert('Você buscou por: ' + query);
+        // Aqui você pode adicionar a lógica para buscar o PDF no servidor
+    } else {
+        alert('Por favor, insira um termo de busca.');
+    }
 }
 
 function filterByCourse() {
